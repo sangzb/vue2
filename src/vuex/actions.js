eb3ce97841a0592a81d935c54b5d4737b062d52d
桑震斌 <sangzhenbin@bamaying.com>
2017-02-24 18:45:46 +0800

    添加登录注册方法

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -4,7 +4,6 @@ export const add = ({commit}, action) => {
   commit('COUNT_ADD', action);
 };
 
-
 //redux与vuex最大的区别在于redux的dispatch可以接受{type: '', action: {}},也可以接受(dispatch) => {}这样的匿名函数
 //vuex只能接受已经存在的action的函数名
 export const getInfo = ( { commit }, action) => {
@@ -14,4 +13,21 @@ export const getInfo = ( { commit }, action) => {
     type: 'GET_INFO'
   })()({ commit }, action);
 };
+
+export const userLogin = ({ commit }, params ) => {
+  return fetch({
+    url: `${C('API_URI')}/login`,
+    method: 'post',
+    type: 'USER_LOGIN'
+  })(params)({ commit }, params);
+};
+
+export const userRegister = ({ commit }, params ) => {
+  return fetch({
+    url: `${C('API_URI')}/register`,
+    method: 'post',
+    type: 'USER_REGISTER'
+  })(params)({ commit }, params);
+};
+
 
--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -1,3 +1,4 @@
+import { C } from '../utils';
 export default {
   COUNT_ADD (state, param) {
     console.log(param);
@@ -10,8 +11,28 @@ export default {
   REQUEST_GET_INFO (state, param) {
     state.isLoading = true;
   },
-
   RECIEVE_GET_INFO (state, param) {
+    state.isLoading = false;
+  },
+
+  REQUEST_USER_LOGIN (state, param) {
+    state.isLoading = true;
+  },
+  RECIEVE_USER_LOGIN (state, param) {
+    state.isLoading = false;
+  },
+
+  REQUEST_USER_REGISTER (state, param) {
+    state.isLoading = true;
+  },
+  RECIEVE_USER_REGISTER (state, param) {
+    state.isLoading = false;
+  },
+
+  REQUEST_USER_EXIST (state, param) {
+    state.isLoading = true;
+  },
+  RECIEVE_USER_EXIST (state, param) {
     state.isLoading = false;
   }
 };
