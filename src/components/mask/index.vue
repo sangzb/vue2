af2f0a820db0004e25a50228044b999cb24dc704
桑震斌 <sangzhenbin@bamaying.com>
2017-02-24 18:45:13 +0800

    复制出mask模块

---
+++ src/components/mask/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="LoadingMaskComponent">
+    <div v-if="message && message.length" class='container'>
+      <span class="text">{{message}}</span>
+    </div>
+    <div v-else class='container'>
+      <div class='loadingImage'></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    data() {
+      return {
+        author: 'sangzb'
+      }
+    },
+    props: ['message'],
+    computed: mapGetters({
+      isLoading: 'getIsLoading'
+    }),
+    mounted() {
+      let $ = window.jQuery;
+      let ww = document.documentElement.clientWidth;
+      let wh = document.documentElement.clientHeight;
+      let text = $('.container span.text');
+      text.css({ left: (ww - text.outerWidth()) / 2, top: (wh - text.outerHeight()) / 2 });
+    }
+  }
+</script>
+
+<style lang='sass'>
+  @import './index.scss';
+</style>
