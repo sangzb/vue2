63ec5b240ec16b164b4d9edb01cdec0240721e89
桑震斌 <sangzhenbin@bamaying.com>
2017-02-24 15:40:33 +0800

    添加loading mask组件

---
+++ src/components/loading/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="LoadingMaskComponent">
+    {{message}}
+    <div v-if="message" class='container'>
+      <span>{{message}}</span>
+    </div>
+    <div v-else class='container'>
+      <div class='loadingImage'></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  export default {
+    data() {
+      return {
+        author: 'sangzb'
+      }
+    },
+    props: ['message'],
+    computed: mapGetters({
+      isLoading: 'getIsLoading'
+    }),
+    mounted() {
+      console.log('loading......')
+    }
+  }
+</script>
+
+<style lang='sass'>
+  @import './index.scss';
+</style>
