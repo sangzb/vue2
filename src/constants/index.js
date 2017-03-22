9c62d7f123f31324de6f49ec2923f54f36023d1d
桑震斌 <sangzhenbin@bamaying.com>
2017-03-22 18:48:15 +0800

    nothing

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_URI = 'http://127.0.0.1:8000';
-//export const API_URI = 'http://45.76.210.139/api';
+//export const API_URI = 'http://127.0.0.1:8000';
+export const API_URI = 'http://45.77.22.206/api';
